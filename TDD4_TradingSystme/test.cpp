453707de09d2fa07b15d58a3c85a71cee5c51047
yenna <yena.gwak@gmail.com>
2025-07-28 10:30:13 +0900

    add Login TC

--- TDD4_TradingSystme/test.cpp
+++ TDD4_TradingSystme/test.cpp
@@ -1,21 +1,50 @@
 #include <iostream>
+#include <stdexcept>
 #include "gmock/gmock.h"
 
 using namespace testing;
+using namespace std;
 
 class TradingSystemFixture : public Test {
 public:
 
 };
 
-TEST_F(TradingSystemFixture, SelectWrongStock) {
+/* Select Stock */
+TEST_F(TradingSystemFixture, Select_Stock_Fail) {
 
+	// EXPECT_THROW( getStock(), runtime_error);
 }
 
-TEST_F(TradingSystemFixture, SelectKiwerSuccess) {
+TEST_F(TradingSystemFixture, Select_Kiwer_Success) {
 
+	// EXPECT_EQ(getStock(), string{"KIWER is selected"});
 }
 
-TEST_F(TradingSystemFixture, SelectNemoSuccess) {
+TEST_F(TradingSystemFixture, Select_Nemo_Success) {
 
+	// EXPECT_EQ(getStock(), string{"NEMO is selected"});
+}
+
+
+
+/* Login */
+TEST_F(TradingSystemFixture, Login_Kiwer_Fail) {
+
+	// EXPECT_THROW( login(), runtime_error);
+}
+
+TEST_F(TradingSystemFixture, Login_Kiwer_Success) {
+
+	// EXPECT_EQ(login(), "KIWER Login Success");
+}
+
+TEST_F(TradingSystemFixture, Login_Nemo_Fail) {
+
+	// EXPECT_THROW( login(), runtime_error);
+}
+
+TEST_F(TradingSystemFixture, Login_Nemo_Success) {
+
+	// EXPECT_EQ(login(), "Nemo Login Success");
 }
